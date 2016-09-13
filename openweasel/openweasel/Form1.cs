6a8f76f69c715d734a915a33c4ee630fd4a5001c
James <james@kngdm.us>
2016-09-13 13:21:55 -0700

    Installation first form.

---
+++ openweasel/openweasel/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace openweasel
+{
+    public partial class OpenWeasel : Form
+    {
+        public OpenWeasel()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWeasel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+           
+        }
+        // Create Form2.
+        public class Form2 : Form
+        {
+            public Form2()
+            {
+                Text = "Form2";
+            }
+        }
+    }
+}
